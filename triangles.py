a097219d94f7bcf6a46fbc7cb1fe8dfc8556407c
hptks <hristotanev7@gmail.com>
2016-04-25 22:31:36 +0100

    Drawing some triangles.

--- triangles.py
+++ triangles.py
@@ -4,27 +4,27 @@ def draw():
     window=turtle.Screen()
     window.bgcolor("black")
 
-    adam=turtle.Turtle()
-    adam.shape("turtle")
-    adam.color("black")
-    adam.goto(-150,-150)
-    adam.color("white")
-    adam.speed(5)
+    turtle=turtle.Turtle()
+    turtle.shape("turtle")
+    turtle.color("black")
+    turtle.goto(-150,-150)
+    turtle.color("white")
+    turtle.speed(5)
     for i in range(1,4):
-        adam.forward(400)
-        adam.left(120)
+        turtle.forward(400)
+        turtle.left(120)
 
     n=200
     for i in range(1,4):
-        adam.forward(n)
-        adam.left(60)
-        adam.forward(n)
-        adam.left(120)
-        adam.forward(n)
-        adam.left(120)
-        adam.forward(n)
+        turtle.forward(n)
+        turtle.left(60)
+        turtle.forward(n)
+        turtle.left(120)
+        turtle.forward(n)
+        turtle.left(120)
+        turtle.forward(n)
         n=n/2
-        adam.left(60)
+        turtle.left(60)
 
     window.exitonclick()
     
